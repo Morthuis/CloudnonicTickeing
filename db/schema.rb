082f987b0bbb875370307640c4990a9728c2e5ca
Moises Higuera <morthuisater@gmail.com>
2024-01-13 01:26:04 -0500

    Creating Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_12_232014) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_061221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.float "ticket_price"
+    t.integer "tickets_threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
