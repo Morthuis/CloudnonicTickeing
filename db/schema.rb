acf7b6185f694df69611c421fb8607b9d1bb4644
Moises Higuera <morthuisater@gmail.com>
2024-01-13 02:02:44 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_13_064146) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_065027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_13_064146) do
     t.integer "tickets_threshold"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_13_064146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
   add_foreign_key "tickets", "events"
 end
