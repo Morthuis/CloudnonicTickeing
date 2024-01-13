7432125052dc27d40bed014f7dc2d28573e6e497
Moises Higuera <morthuisater@gmail.com>
2024-01-13 14:25:59 -0500

    Callback created to recalculate totalprice for tickets

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ApplicationRecord
+  after_update :recalculate_total_price_for_users, if: :ticket_price_changed?
   has_many :tickets
   belongs_to :user
+
+
+
+  private
+
+  def ticket_price_changed?
+    saved_change_to_ticket_price?
+  end
+
+  def recalculate_total_price_for_users
+    print ">>>>>>>>>>>>>>>>#{self.ticket_price}<<<<<<<<<<<<<<<<<<<<"
+  end
 end
