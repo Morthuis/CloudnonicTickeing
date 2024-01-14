4a1a593043e4e888508d6421b4defc893faad44f
Moises Higuera <morthuisater@gmail.com>
2024-01-14 17:15:13 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+for x in 1..5
+  User.create!(email: "morthuisater#{x}@gmail.com", password: 'qwer1234', password_confirmation: 'qwer1234')
+  for a in 1..12
+    Event.create!(name: "Event#{a} #{x}", description: "Enjoy so much this event #{a} #{x}",start_time: Time.now, end_time: Time.now, ticket_price: 15 ,tickets_threshold: 800, user_id: x)
+      for t in 1..10
+        Ticket.create!(buyer_name: "Moises #{x}", quantity: t + 2, event_id: a, user_id: x)
+      end
+  end
+end
